1c3a4ce20ed67605935ad4febe6f9352db9866fc
Alanquintana0 <65621524+Alanquintana0@users.noreply.github.com>
2024-08-12 16:27:34 -0600

    Se completo index

--- controllers/cases.js
+++ controllers/cases.js
@@ -4,5 +4,14 @@ const { validationCase } = require('helper/validate')
 const fs = require('fs');
 const path = require('path');
 
+const list = (req, res, next) => {
+    Case.find().sort({_initDate: -1}).then(objs => res.status(200).json({
+        message: "Cases list retrieved",
+        obj: objs
+    })).catch(ex => res.status(500).json({
+        message: "Error",
+        obj: ex
+    }));
+}
 
 
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const { connection } = require("./database/connection");
 const express = require("express");
 const cors = require("cors");
+
+const caseRouter = require('./router/cases');
+
+connection();
+
+const app = express();
+const port = 3900;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use('/articles', caseRouter);
+
+app.get("/", (req, res) =>{
+    return res.status(200).json({
+        message: "Welcome to my api"
+    })
+});
+
+app.listen(port, () => {
+    console.log("App running on port: " + port);
+})
